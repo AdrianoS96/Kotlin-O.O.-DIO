29761f87ae1997fd8ab0903b739cb5709f4ef3c7
Adriano S96 <adrianodes96@gmail.com>
2021-03-09 18:38:26 -0300

    mudança na classe pessoa e funcionário para o tipo ABSTRACT

--- src/main/kotlin/one/digitalinnovation/digiobank/testes/TesteFuncionario.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/testes/TesteFuncionario.kt
@@ -5,10 +5,10 @@ import one.digitalinnovation.digiobank.Pessoa
 import java.math.BigDecimal
 
 fun main() {
-    val adriano = Pessoa(nome = "Adriano", cpf = "123.123.123-12")
+   val adriano = Pessoa(nome = "Adriano", cpf = "123.123.123-12")
 
-    println(adriano.nome)
-    println(adriano.cpf)
+   println(adriano.nome)
+   println(adriano.cpf)
 
     val rita = Funcionario("Rita","113.113.113-11", BigDecimal.valueOf(1000.0))
 
