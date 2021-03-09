3d0032701a4a48f4625dd7999a78ed7bbb49d15f
Adriano S96 <adrianodes96@gmail.com>
2021-03-09 14:25:51 -0300

    Criação da classe pessoa e adicionado o método private set para o atributo cpf

--- src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -1,15 +1,24 @@
 package one.digitalinnovation.digiobank
 
-class Pessoa {
+class Pessoa { //alter class
 
     var  nome:String = "Adriano"
     var cpf:String ="123.123.123-12"
+    private set  // utilizado para deixar o método set do atributo acima em modo PRIVATE
+/**
+ * possibilidade de criar a inner class
+ * uma espécie de subclass
+
+    inner class Endereco{
+        var rua:String = "Rua teste"
+    }
+**/
 
 }
 fun main(){
 
     val adriano = Pessoa()
-
+    adriano.nome = "djajla"
     println(adriano.nome)
     println(adriano.cpf)
 
