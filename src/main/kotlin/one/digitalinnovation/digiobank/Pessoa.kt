8e430abf97c39462d7b614ff366c244bb81981ed
Adriano S96 <adrianodes96@gmail.com>
2021-03-09 14:39:30 -0300

    criação de uma função na classe pessoa. construtor secundario (comentado)

--- src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -5,6 +5,10 @@ class Pessoa { //alter class
     var  nome:String = "Adriano"
     var cpf:String ="123.123.123-12"
     private set  // utilizado para deixar o método set do atributo acima em modo PRIVATE
+
+    // constructor()
+
+    fun pessoaInfo() = "Nome: $nome \nCPF: $cpf"
 /**
  * possibilidade de criar a inner class
  * uma espécie de subclass
@@ -18,8 +22,7 @@ class Pessoa { //alter class
 fun main(){
 
     val adriano = Pessoa()
-    adriano.nome = "djajla"
-    println(adriano.nome)
-    println(adriano.cpf)
+
+    println(adriano.pessoaInfo())
 
 }
